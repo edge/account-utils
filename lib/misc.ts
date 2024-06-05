c7525d351215769556f054fa709fee2f259df034
Aneurin Barker Snook <a@aneur.in>
2024-06-05 09:51:09 +0100

    feat: add config api

--- lib/misc.ts
+++ lib/misc.ts
@@ -1,9 +1,62 @@
 import { RequestCallback } from '.'
 import superagent from 'superagent'
 
+export interface GetConfigResponse {
+  billing: {
+    minimumBillableUnit: number
+  }
+  cdn: {
+    dataCost: number
+    dataCostBasis: number
+    minimumCost: number
+    minimumCostBasis: number
+    minimumTTL: number
+    requestCost: number
+    requestCostBasis: number
+  }
+  dns: {
+    cost: number
+    costBasis: number
+    nameservers: string[]
+    recordTypes: string[]
+  }
+  server: {
+    limit: {
+      bandwidth: {
+        min: number
+        max: number
+      }
+      cpu: {
+        min: number
+        max: number
+      }
+      disk: {
+        min: number
+        max: number
+      }
+      ram: {
+        min: number
+        max: number
+      }
+    }
+  }
+  suspend: {
+    threshold: number
+  }
+  warning: {
+    threshold: number
+  }
+}
+
 export interface ParseDomainResponse {
   domain: string
   apex: boolean
+}
+
+export async function getConfig(host: string, cb?: RequestCallback): Promise<GetConfigResponse> {
+  const req = superagent.get(`${host}/config`)
+  const res = await cb?.(req) || await req
+  return res.body
 }
 
 /** Parse a domain to determine whether it is an apex or subdomain. */
