5e5dc1a649083dd97bb16970941f8062cb9c1af1
Aneurin Barker Snook <a@aneur.in>
2024-07-02 10:45:40 +0100

    docs: add storage node typedocs

--- lib/storage.ts
+++ lib/storage.ts
@@ -75,6 +75,15 @@ export interface UpdateStorageNodeResponse {
 
 export type UploadStorageFileResponse = File
 
+/**
+ * Create a [folder] node in a storage integration.
+ *
+ * @param host API host URL
+ * @param token Bearer token
+ * @param key Integration key
+ * @param path Path to [folder] node
+ * @return New node data
+ */
 export async function createStorageNode(host: string, token: string, key: string, path: string, cb?: RequestCallback): Promise<CreateStorageNodeResponse> {
   const req = superagent.post(`${host}/storage/${key}/fs/${path.replace(/^\//, '')}`).set('Authorization', `Bearer ${token}`)
   const res = await cb?.(req) || await req
@@ -88,6 +97,16 @@ export async function deleteStorageFiles(host: string, token: string, key: strin
   return res.body
 }
 
+/**
+ * Delete a node.
+ * If the node is a folder, then its child nodes are deleted automatically by API.
+ *
+ * @param host API host URL
+ * @param token Bearer token
+ * @param key Integration key
+ * @param path Path to node
+ * @return Old node data
+ */
 export async function deleteStorageNode(host: string, token: string, key: string, path: string, cb?: RequestCallback): Promise<DeleteStorageNodeResponse> {
   const req = superagent.delete(`${host}/storage/${key}/fs/${path.replace(/^\//, '')}`).set('Authorization', `Bearer ${token}`)
   const res = await cb?.(req) || await req
@@ -102,6 +121,16 @@ export async function getStorageFiles(host: string, token: string, key: string, 
   return res.body
 }
 
+/**
+ * Get a node.
+ * If the node is a folder, then its child nodes are also retrieved in a list, subject to the `params` provided.
+ *
+ * @param host API host URL
+ * @param token Bearer token
+ * @param key Integration key
+ * @param path Path to node
+ * @param params Query parameters for child nodes
+ */
 // eslint-disable-next-line max-len
 export async function getStorageNode(host: string, token: string, key: string, path: string, params?: GetStorageNodeParams, cb?: RequestCallback): Promise<GetStorageNodeResponse> {
   const req = superagent.get(`${host}/storage/${key}/fs/${path.replace(/^\//, '')}`).set('Authorization', `Bearer ${token}`)
@@ -117,6 +146,17 @@ export async function updateStorageFiles(host: string, token: string, key: strin
   return res.body
 }
 
+/**
+ * Update a node.
+ * If the node is a folder, then its child nodes will be updated automatically by API as applicable to the update.
+ *
+ * @param host API host URL
+ * @param token Bearer token
+ * @param key Integration key
+ * @param path Path to node
+ * @param data Node update data
+ * @return Updated node data
+ */
 // eslint-disable-next-line max-len
 export async function updateStorageNode(host: string, token: string, key: string, path: string, data: UpdateStorageNodeRequest, cb?: RequestCallback): Promise<UpdateStorageNodeResponse> {
   const req = superagent.put(`${host}/storage/${key}/fs/${path.replace(/^\//, '')}`).set('Authorization', `Bearer ${token}`).send(data)
@@ -125,7 +165,14 @@ export async function updateStorageNode(host: string, token: string, key: string
 }
 
 /**
- * Upload a file to a Gateway (**not** the Account API).
+ * Upload a file to a Gateway (**not** directly to the Account API).
+ *
+ * @param host Gateway URL (**not** API base URL)
+ * @param apiKey Integration API key
+ * @param key Integration key
+ * @param path Path to file
+ * @param file File data
+ * @return Gateway response with UUID
  */
 // eslint-disable-next-line max-len
 export async function uploadStorageFile(gateway: string, apiKey: string, key: string, path: string, file: string, cb?: RequestCallback): Promise<UploadStorageFileResponse> {
